46ff1d5b9a2a190ee530b2effbd69d64e07d7920
Abdoul-Madjid17 <154878306+Abdoul-Madjid17@users.noreply.github.com>
2025-05-04 00:02:30 +0600

    Update db.ts

--- api/shared/db.ts
+++ api/shared/db.ts
@@ -1,1 +1,10 @@
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGO_URI!);
+
+export async function getCollection<T>(collectionName: string) {
+  if (!client.isConnected?.()) await client.connect();
+  const db = client.db(process.env.MONGO_DB_NAME);
+  return db.collection<T>(collectionName);
+}
 
